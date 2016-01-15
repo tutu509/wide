35c99c7944e237d69a5c0a0e93b6e5c127a831c7
tutu <tutu509@126.com>
2016-01-15 22:14:38 +0800

    The first push
    Signed-off-by: tutu <tutu509@126.com>

---
+++ 堆栈距离/单向堆栈距离计算总和/stack-add.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import sys
+import re
+import os
+inFilename = sys.argv[1]
+if os.path.isfile(inFilename):
+    namelength = inFilename.rfind(".")
+    name = inFilename[0:namelength]
+    exten = inFilename[namelength:]
+    outFilename = name + "_in_new" + exten
+
+print "inFilename:", inFilename
+print "outFilename:", outFilename
+
+
+fpRead = open(inFilename, "r")
+fpWrite = open(outFilename, "w+")
+
+EndPattern = re.compile(r'end')
+
+s = '0'
+
+tag = 1
+while bool(tag):
+    linePattern1 = re.compile(r'(%s)\s.*\s([0-9]+)'% s)
+    tag = 0
+    i = 0
+    fpRead.seek(0)
+    lines = fpRead.readline()
+    while lines:
+
+        match1 = linePattern1.search(lines)
+        endmatch = EndPattern.search(lines)
+        if match1:
+
+            i = int(match1.group(2))+i
+            tag = 1 
+
+        lines = fpRead.readline()
+    fpWrite.write("%s  %d\n" %(s,i))
+    s = str(int(s)+1)
+fpRead.close()
+fpWrite.close()
---
+++ 堆栈距离/推展距离数据提取/in_out_order.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+import sys
+import re
+import os
+inFilename = sys.argv[1]
+if os.path.isfile(inFilename):
+	namelength = inFilename.rfind(".")
+	name = inFilename[0:namelength]
+	exten = inFilename[namelength:]
+	outFilename = name+"-collect-special"+exten
+
+print "inFilename:", inFilename
+print "outFilename:", outFilename
+
+fpRead = open(inFilename, "r")
+fpWrite = open(outFilename, "w+")
+
+switchdtbPattern = re.compile(r'(Stack_Distance_Distribution_In_Order::)(\d.*?\s).* (\s[0-9|\.]+)')
+stackoutPattern = re.compile(r'(dcache.Out_Of_Stack_Distance_Distribution::)(\d.*?\s).*(\s[0-9|\.]+)')
+threadbeginPattern = re.compile(r'Begin Simulation Statistics')
+threadendPattern = re.compile(r'End Simulation Statistics')
+lines = fpRead.readline()
+def stack(a):
+    intval = int(a)
+    diff = intval-last
+    i = 0
+    while (diff > i):
+        print last+1,0
+        i = i+1
+    last = intval
+    return
+
+while lines:
+#	linesmatch = linesPatter.match(lines)
+	threadbeginmatch = threadbeginPattern.search(lines)
+#	threadendmatch = threadendPattern.match(lines)
+        #lineswrite = linesmatch.group(1)
+        # print "----------------------- reading lines------------------"
+	if threadbeginmatch:
+                last1 = 0
+                last2 = 0
+                print "------------------------ entering thread -------------------"
+		threadlines = fpRead.readline()
+#                threadendmatch = threadendPattern.match(threadlines)
+		while threadlines:
+#           print threadlines
+			stackinmatch = switchdtbPattern.search(threadlines)
+                        stackoutmatch = stackoutPattern.search(threadlines)
+                        threadendmatch = threadendPattern.search(threadlines)
+                        if stackinmatch:
+                            intval = int(stackinmatch.group(2))
+                            diff = intval-last1
+                            i = 1
+                            while (diff > i):
+                                print "%d   0"% (last1+i)
+                                fpWrite.write("%d   0\n" % (last1+i))
+                                i = i+1
+                            last1 = intval
+                            print stackinmatch.group(2),stackinmatch.group(3)
+                            fpWrite.write("%s %s\n" %(stackinmatch.group(2),stackinmatch.group(3)))
+                        if stackoutmatch:
+                            intval = int(stackoutmatch.group(2))
+                            diff = intval-last2
+                            i = 1
+                            while (diff > i):
+                                print "%d   0"% (last2+i)
+                                fpWrite.write("%d   0\n" % (last2+i))
+                                i = i+1
+                            last2 = intval
+                            print stackoutmatch.group(2),stackoutmatch.group(3)
+                            fpWrite.write("%s %s\n" %(stackoutmatch.group(2),stackoutmatch.group(3)))
+			if threadendmatch:
+			#	fpWrite.write("\n")
+                            print "------------------------ thread collection done!! --------------------"
+                            fpWrite.write("------------------------------------------------------\n")
+                            break
+                        threadlines = fpRead.readline()
+        lines = fpRead.readline()
+fpRead.close()
+fpWrite.close()
+
