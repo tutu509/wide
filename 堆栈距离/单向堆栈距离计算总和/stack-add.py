c5228b379d40e17e61a3d38eedc3d5947a583b6e
tutu <tutu509@126.com>
2016-01-21 20:09:19 +0800

    handle both inorder and outorder simultaneously

--- 堆栈距离/单向堆栈距离计算总和/stack-add.py
+++ 堆栈距离/单向堆栈距离计算总和/stack-add.py
@@ -2,42 +2,73 @@
 import sys
 import re
 import os
-inFilename = sys.argv[1]
-if os.path.isfile(inFilename):
-    namelength = inFilename.rfind(".")
-    name = inFilename[0:namelength]
-    exten = inFilename[namelength:]
-    outFilename = name + "_in_new" + exten
+inFilename1 = sys.argv[1]
+inFilename2 = sys.argv[2]
+if os.path.isfile(inFilename1):
+    namelength = inFilename1.rfind(".")
+    name = inFilename1[0:namelength]
+    exten = inFilename1[namelength:]
+    outFilename1 = name + "_total" + exten
 
-print "inFilename:", inFilename
-print "outFilename:", outFilename
+if os.path.isfile(inFilename2):
+    namelength = inFilename2.rfind(".")
+    name = inFilename2[0:namelength]
+    exten = inFilename2[namelength:]
+    outFilename2 = name + "_total" + exten
 
 
-fpRead = open(inFilename, "r")
-fpWrite = open(outFilename, "w+")
+print "inFilename:", inFilename1,inFilename2
+print "outFilename:", outFilename1,outFilename2
 
-EndPattern = re.compile(r'end')
+
+inRead = open(inFilename1, "r")
+outRead = open(inFilename2, "r")
+inWrite = open(outFilename1, "w+")
+outWrite = open(outFilename2, "w+")
+
 
 s = '0'
 
 tag = 1
 while bool(tag):
-    linePattern1 = re.compile(r'(%s)\s.*\s([0-9]+)'% s)#把变量s作为匹配pattern
+    linePattern1 = re.compile(r'(%s)\s.*\s([0-9]+)'% s)
     tag = 0
     i = 0
-    fpRead.seek(0)
-    lines = fpRead.readline()
+    inRead.seek(0)
+    lines = inRead.readline()
     while lines:
 
         match1 = linePattern1.search(lines)
-        endmatch = EndPattern.search(lines)
         if match1:
 
             i = int(match1.group(2))+i
             tag = 1 
 
-        lines = fpRead.readline()
-    fpWrite.write("%s  %d\n" %(s,i))
+        lines = inRead.readline()
+    inWrite.write("%s  %d\n" %(s,i))
     s = str(int(s)+1)
-fpRead.close()
-fpWrite.close()
+
+s = '0'
+
+tag = 1
+while bool(tag):
+    linePattern1 = re.compile(r'(%s)\s.*\s([0-9]+)'% s)
+    tag = 0
+    i = 0
+    outRead.seek(0)
+    lines = outRead.readline()
+    while lines:
+
+        match1 = linePattern1.search(lines)
+        if match1:
+
+            i = int(match1.group(2))+i
+            tag = 1 
+
+        lines = outRead.readline()
+    outWrite.write("%s  %d\n" %(s,i))
+    s = str(int(s)+1)
+inRead.close()
+outRead.close()
+inWrite.close()
+outWrite.close()
