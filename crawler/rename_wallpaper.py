67003da4b3bbaff1b038ed7015c8dab8f8ef4a8d
tutu <tutu509@126.com>
2016-04-11 12:37:31 +0800

    add path check
    Signed-off-by: tutu <tutu509@126.com>

--- crawler/rename_wallpaper.py
+++ crawler/rename_wallpaper.py
@@ -6,6 +6,7 @@ files=os.listdir(path)
 a='1'
 for item in files:
 	if item[item.find('.'):]!='.py':
+		while os.path.exists(a+'.jpg'):
+			a=str(int(a)+1)
 		os.rename(item,a+'.jpg')
-		a=str(int(a)+1)
 		print a
